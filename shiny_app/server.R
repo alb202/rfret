d22dfd31d2429f562dc8f2350359454d6fd40156
Andrew Blumenfeld <ablumenf@gmail.com>
2017-05-23 16:31:00 -0600

    Uploading UI progress - bulk file input works but nothing else

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,38 +1,44 @@
 library(shiny)
-
+library(gridExtra)
+library(ggplot2)
+i = 1
 function(input, output) {
-    output$contents <- renderTable({
-
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, it will be a data frame with 'name',
-        # 'size', 'type', and 'datapath' columns. The 'datapath'
-        # column will contain the local filenames where the data can
-        # be found.
-
-        #inFile <- input$input_files
-
-        if (is.null(input$input_files))
-            return(NULL)
-        file_list <- c("")
+    source("../R/inspect_raw_data.R")
+    file_list <- c("")
+    reactive_data <- renderText({
         for(i in 1:length(input$input_files[,1]))
         {
-            #lst[[i]] <- read.csv(input$file1[[i, 'datapath']])
-            #lst[i] <- read.csv(input$file1[[i, 'datapath']])
-            #print(colnames(read.csv(input$file1[i, 'datapath'])))
-            print(read.csv(input$input_files[i, 'datapath'],
-                           header=input$header,
-                           sep=input$sep,
-                           quote=input$quote,
-                           skip = 4)
-                  )
-            print(input$input_files$name[i])
-            new_row <- c(input$input_files$name[i],
-                         read.csv(input$input_files[i, 'datapath'],
+            new_dataframe <- read.csv(input$input_files[i, 'datapath'],
                                   header=input$header,
                                   sep=input$sep,
                                   quote=input$quote,
-                                  skip = 4))
-            file_list <- c(file_list, new_row)
+                                  skip = input$skip_rows)
+            comment(new_dataframe) <- input$input_files$name[i]
+            print(class(new_dataframe))
+            if(input$header == FALSE){
+                colnames(new_dataframe) <- c("Well Row","Well Col","Content",
+                                          "fret_channel","donor_channel",
+                                          "acceptor_channel","concentration")
+            }
+            file_list <- c(file_list, new_dataframe)
         }
+        output$file_list <- file_list
     })
+
+    # output$plotgraph <- renderPlot({
+    #     raw_list <- reactive_data[1]
+    #     raw_plots <- inspect_raw_data(raw_list)
+    #     fig1 <- reactive({raw_plots$donor})
+    #     fig2 <- reactive({raw_plots$acceptor})
+    #     fig3 <- reactive({raw_plots$fret})
+    #     fig_list <- list(fig1(),fig2(),fig3())
+    #     to_delete <- !sapply(fig_list,is.null)
+    #     fig_list <- fig_list[to_delete]
+    #     if (length(ptlist)==0){
+    #         return(NULL)
+    #     }
+    # grid.arrange(grobs=ptlist,ncol=length(ptlist))
+
 }
+
+
