45e078a0775cad44a2e050adb0f0e061baff1424
Andrew Blumenfeld <ablumenf@gmail.com>
2017-05-23 22:38:02 -0600

    The app can load the files 1 at a time, and display the file name and the raw plots

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,29 +1,59 @@
-library(shiny)
-library(gridExtra)
+## SERVER.R
 library(ggplot2)
-i = 1
+library(gridExtra)
+source("/media/ab/HD4/rfret/R/inspect_raw_data.R")
+
 function(input, output) {
-    source("../R/inspect_raw_data.R")
-    file_list <- c("")
-    reactive_data <- renderText({
-        for(i in 1:length(input$input_files[,1]))
-        {
-            new_dataframe <- read.csv(input$input_files[i, 'datapath'],
-                                  header=input$header,
-                                  sep=input$sep,
-                                  quote=input$quote,
-                                  skip = input$skip_rows)
-            comment(new_dataframe) <- input$input_files$name[i]
-            print(class(new_dataframe))
-            if(input$header == FALSE){
-                colnames(new_dataframe) <- c("Well Row","Well Col","Content",
-                                          "fret_channel","donor_channel",
-                                          "acceptor_channel","concentration")
-            }
-            file_list <- c(file_list, new_dataframe)
+    infile <- reactive({
+        if (is.null(input$data_file)) {
+            # User has not uploaded a file yet
+            return(NULL)
         }
-        output$file_list <- file_list
+        objectsLoaded <- list()
+        for(i in 1:length(input$data_file$name)){
+            df <- read.csv(input$data_file[i, 'datapath'],
+                           header=input$header,
+                           sep=input$sep,
+                           quote=input$quote,
+                           skip = input$skip_rows)
+            df <- list(input$data_file$name, df)
+            objectsLoaded[[length(objectsLoaded)+1]] <- df
+        }
+        return(objectsLoaded)
     })
+
+    myData <- reactive({
+        df<-infile()
+        if (is.null(df)) return(NULL)
+        return(df)
+    })
+    output$filename <-renderText({myData()[[1]][[1]]})
+    output$raw_output <- renderPlot({
+        if(is.null(input$data_file)) return(print("Please upload some FRET data"))
+        df <- data.frame(myData()[[1]][[2]])
+        raw_data_plots <- inspect_raw_data(df)
+        raw_data_grid <- grid.arrange(raw_data_plots$donor, raw_data_plots$acceptor, raw_data_plots$fret, ncol=2, nrow=3)
+    })
+}
+
+    #output$
+
+    # output$plotgraph <- renderPlot({
+    #     raw_plots <- inspect_raw_data(new_data$file_list[1])
+    #     print("test")
+    #     fig1 <- raw_plots$donor
+    #     fig2 <- raw_plots$acceptor
+    #     fig3 <- raw_plots$fret
+    #     fig_list <- list(fig1,fig2,fig3)
+    #     to_delete <- !sapply(fig_list,is.null)
+    #     fig_list <- fig_list[to_delete]
+    #     if (length(fig_list)==0){
+    #         return(NULL)
+    #     }
+    #     print(fig1)
+        #grid.arrange(grobs=fig_list,ncol=length(fig_list))
+    #})
+    #print("test444")
 
     # output$plotgraph <- renderPlot({
     #     raw_list <- reactive_data[1]
@@ -38,7 +68,3 @@ function(input, output) {
     #         return(NULL)
     #     }
     # grid.arrange(grobs=ptlist,ncol=length(ptlist))
-
-}
-
-
