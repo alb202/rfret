a5a0c4da95062efcaee00b25b4713f362eff954c
Andrew Blumenfeld <ablumenf@gmail.com>
2017-07-02 23:34:53 -0600

    Updated server.R to work with new vectorized data. Full pipeline does not work yet

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -5,10 +5,10 @@ library(gridExtra)
 library(magrittr)
 library(readr)
 library(shinythemes)
-source("../R/inspect_raw_data.R")
-source("../R/format_data.R")
-source("../R/average_technical_replicates.R")
-source("../R/correct_fret_signal.R")
+source("../R/fret_inspect_raw_data.R")
+source("../R/fret_format_data.R")
+source("../R/fret_average_replicates.R")
+source("../R/fret_correct_signal.R")
 source("../R/fit_binding_model.R")
 source("../R/make_figure.R")
 source("../R/guess_parameters.R")
@@ -35,18 +35,34 @@ server <- function(input, output, session) {
             objectsLoaded[[ input$data_file$name[i] ]] <- df
         }
         values$file_index <- 1
-        values$number_of_files <- length(objectsLoaded)
+        #values$number_of_files <- length(objectsLoaded)
+        print("number of files: ")
+        print(values$number_of_files)
         values$dataset_decisions <- rep(NA, length(objectsLoaded))
-        return(objectsLoaded)
+        values$dataset_names <- sub(names(objectsLoaded),
+                                    pattern = ".csv",
+                                    replacement = "")
+        #print(objectsLoaded[[1]])
+        print(length(objectsLoaded))
+        print(names(objectsLoaded))
+        print(class(objectsLoaded[1]))
+        print(class(objectsLoaded[[1]]))
+        ffd <- fret_format_data(input = objectsLoaded)
+        return(ffd)
+        #return(objectsLoaded)
     })
 
+
+
     myData <- reactive({
-        df<-input_files()
-        if (is.null(df)) return(NULL)
+        ffd <-input_files()
+        if (is.null(ffd)) return(NULL)
+        ird <- fret_inspect_raw_data(raw_data = ffd, plot_format = "png")
+        print(class(ird))
         #hideElement("splash_screen")
         #showElement("raw_output")
         #showElement("accept")
-        values$number_of_files <- length(df)
+        values$number_of_files <- length(ird)
         updateTabsetPanel(session = session, inputId = "sidebar", selected = "advanced")
         updateTabsetPanel(session = session, inputId = "main", selected = "inspect")
         #enable(id = "next1")
@@ -58,18 +74,18 @@ server <- function(input, output, session) {
         #    values$dataset_decisions[1:length(values$dataset_decisions)] <- TRUE
         #    #input$process_all <- TRUE
         #}
-        return(df)
+        return(ird)
     })
     output$filename <- renderText({
         if(is.null(input$data_file)) return(NULL)
-        paste(values$file_index, "-    ", {names(myData())[[values$file_index]]})
+        paste(values$file_index, " - ", values$dataset_names[[values$file_index]])
     })
 
     output$raw_output <- renderPlot(height = 800, {
         if(is.null(input$data_file)) return(NULL)
         #showElement("accept")
-        df <- data.frame(myData()[[values$file_index]])
-        raw_data_plots <- inspect_raw_data(df)
+        #df <- data.frame(myData()[[values$file_index]])
+        raw_data_plots <- myData()[[values$file_index]]
         raw_data_grid <- grid.arrange(raw_data_plots$fret, raw_data_plots$donor, raw_data_plots$acceptor, nrow=3, ncol=1)
         return(raw_data_grid)
     })
@@ -126,14 +142,29 @@ server <- function(input, output, session) {
         #print(input$skip_rows)
         #raw_data <- format_data(input = lapply(myData()[values$dataset_decisions], "[[", 2), skip_lines = 0)
         #df_list <- myData()[values$dataset_decisions]
-        df_list <- myData()[values$dataset_decisions]
+        ffd <- input_files()
+        print(values$dataset_names)
+        print(values$dataset_decisions)
+        print(values$dataset_names[values$dataset_decisions])
+        ffd <- ffd[ffd$Experiment %in% values$dataset_names[values$dataset_decisions], ]
+        figure <- ffd %>%
+            fret_average_replicates() %>%
+            fret_correct_signal(output_directory = "./corrected_data") %>%
+            fit_binding_model(binding_model = "quadratic",
+                              probe_concentration = 5,
+                              output_directory = "./fit_results") %>%
+            make_figure(probe_concentration = 5,
+                        output_directory = "./final_figures",
+                        plot_format = "png")
+
+
         #df_names <- names(myData())[values$dataset_decisions]
         #print(df_names)
         #names(df_list) <- df_names
-        print(df_list)
-        print(names(df_list))
-        raw_data <- format_data(input = myData()[values$dataset_decisions], skip_lines = 0)
-        print(raw_data)
+        print(ffd)
+        print(names(ffd))
+        #raw_data <- format_data(input = myData()[values$dataset_decisions], skip_lines = 0)
+        #print(raw_data)
         #print(class(df_list))
         #print(length(df_list))
         #names(df_list) <- input$data_file$name[values$dataset_decisions]
@@ -144,30 +175,30 @@ server <- function(input, output, session) {
         #print(names(df_list))
         #raw_data <- format_data(input = df_list, skip_lines = 0)
         #print(raw_data)
-        atr <- average_technical_replicates(raw_data = raw_data)
-        print(atr)
-        cfs <- correct_fret_signal(atr)
+        #atr <- average_technical_replicates(raw_data = raw_data)
+        #print(atr)
+        #cfs <- correct_fret_signal(atr)
         #print(cfs)
         #params <-  guess_parameters(cfs)
         #print(params)
         #print(input$algorithm)
         #print(input$donor_concentration)
         #print(input$hill_coefficient)
-        if(input$algorithm == "hyperbolic"){
-            donor_concentration <- input$donor_concentration
-            hill_coefficient <- FALSE
-        } else{
-            donor_concentration <- NULL
-            hill_coefficient <- input$hill_coefficient
-        }
-        fbm <- fit_binding_model(data_to_fit=cfs, model=input$algorithm, donor_concentration=donor_concentration, fit_Hill_coef=hill_coefficient)
-        print("fbm$fit")
-        print(fbm)
-        print(names(fbm))
-        print(cfs)
-        print(names(cfs))
+        # if(input$algorithm == "hyperbolic"){
+        #     donor_concentration <- input$donor_concentration
+        #     hill_coefficient <- FALSE
+        # } else{
+        #     donor_concentration <- NULL
+        #     hill_coefficient <- input$hill_coefficient
+        # }
+        # fbm <- fit_binding_model(data_to_fit=cfs, model=input$algorithm, donor_concentration=donor_concentration, fit_Hill_coef=hill_coefficient)
+        # print("fbm$fit")
+        # print(fbm)
+        # print(names(fbm))
+        # print(cfs)
+        # print(names(cfs))
         #fig <- make_figure(corrected_data = cfs, fit = fbm)
-        #output$processed_output <- renderPlot({ return(splash_screen()) })
+        output$processed_output <- renderPlot({ figure })
     })
     process_all_listener <- reactive({input$process_all})
 
@@ -188,7 +219,7 @@ server <- function(input, output, session) {
             toggleState(id = "previous", condition = ((values$file_index > 1)))
             toggleState(id = "accept", condition = (!is.null(input$data_file)))
             toggleState(id = "reject", condition = (!is.null(input$data_file)))
-            #toggleElement(id = "accept_all_subsequent", condition = (!is.null(input$data_file)))
+            toggleState(id = "accept_all", condition = (!is.null(input$data_file)))
             #toggleElement(id = "accept_all", condition = (!is.null(input$data_file)))
             if(!is.na(values$dataset_decisions[[values$file_index]])){
                 if(values$dataset_decisions[[values$file_index]] == TRUE){
