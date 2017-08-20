c2f8d61ae0082a55017ffe5f84498fdd5e454316
Andrew Blumenfeld <ablumenf@gmail.com>
2017-08-20 16:24:46 -0600

    Rearranged the "Plan Experiment" buttons

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -4,7 +4,12 @@ library(shinyjs)
 ui <- fluidPage(
     shinyjs::useShinyjs(),
     tags$head(
-        tags$link(rel = "stylesheet", type = "text/css", href = "yeti.css")),
+        tags$link(rel = "stylesheet", type = "text/css", href = "yeti.css"),
+        tags$style(
+            HTML('
+                #conc_required{color: red;font-size: 12px;}
+                #plan_download{width: 140px;font-size: 14px;}
+                 '))),
     titlePanel(strong("RFret - Rapid FRET Analysis in R"), windowTitle = "RFret - Rapid FRET Analysis in R"),
     sidebarLayout(
         sidebarPanel(width = 3,
@@ -56,11 +61,8 @@ ui <- fluidPage(
                                                          numericInput('donor_concentration',
                                                                       'Donor Concentration (required if algorithm is quadratic)',
                                                                       value = NULL),
-                                                         tags$style("#conc_required{color: red;
-                                                                             font-size: 12px;
-                                                                             }"),
-                                                                    hidden(textOutput(outputId="conc_required"))
-                                                         )))),
+                                                         hidden(textOutput(outputId="conc_required"))
+                                                  )))),
                                  tabPanel(title = "Help", value = "help", icon = icon("question-sign", lib="glyphicon"),
                                           tags$hr(),
                                           h5("Help information")
@@ -87,11 +89,11 @@ ui <- fluidPage(
                                                   #fluidRow(align="center", disabled(actionButton(inputId = "next1", label ="", icon = icon("arrow-down", lib = "glyphicon")))),
                                                   fluidRow(div(style = "height:5px;")),
                                                   fluidRow(align="left", div(style = "display:inline-block;content-align:left;",
-                                                      column(2, div(style = "align-content:left;background-color:none;", #width:5px; align="left",
-                                                                    div(style = "height:7px;"),
-                                                                    hidden(plotOutput(inline=FALSE, outputId = "decision_indicator")))),
-                                                      column(2, div(style = "align-content:left;background-color:none;",#style = "align-content:left;width:10px;background-color:none;",
-                                                                    hidden(uiOutput(inline=FALSE,'file_selector')))) #display:inline-block;align:left;
+                                                                             column(2, div(style = "align-content:left;background-color:none;", #width:5px; align="left",
+                                                                                           div(style = "height:7px;"),
+                                                                                           hidden(plotOutput(inline=FALSE, outputId = "decision_indicator")))),
+                                                                             column(2, div(style = "align-content:left;background-color:none;",#style = "align-content:left;width:10px;background-color:none;",
+                                                                                           hidden(uiOutput(inline=FALSE,'file_selector')))) #display:inline-block;align:left;
                                                   ))),
                                            column(11,
                                                   plotOutput(outputId = "raw_output", width = "100%", height = "100%")),
@@ -117,38 +119,53 @@ ui <- fluidPage(
                                            fluidRow(column(width=12, offset=0))
                                        )),
                               tabPanel(title = "Corrected Data", value = "corrected", icon = icon("pencil", lib="glyphicon"),
-                                                   wellPanel(
-                                                       #column(10,tableOutput(outputId = "corrected_data_output")),
-                                                       #column(10,verbatimTextOutput(outputId = "corrected_data_output")),
-                                                       column(10,uiOutput(outputId = "corrected_data_output")),
-                                                       fluidRow(column(width=12, offset=0))
-                                                   )
-                                       ),
+                                       wellPanel(
+                                           #column(10,tableOutput(outputId = "corrected_data_output")),
+                                           #column(10,verbatimTextOutput(outputId = "corrected_data_output")),
+                                           column(10,uiOutput(outputId = "corrected_data_output")),
+                                           fluidRow(column(width=12, offset=0))
+                                       )
+                              ),
                               tabPanel(title = "Plan the Experiment", value = "plan", icon = icon("list-alt", lib="glyphicon"),
                                        wellPanel(
-                                           column(10, numericInput('plan_kd',
-                                                                   'Kd',
-                                                                   NULL,
-                                                                   min = 0),
-                                                  radioButtons('plan_algorithm',
-                                                               'Select the algorithm for fitting the binding model:',
-                                                               c('Hyperbolic'='hyperbolic','Hyperbolic (with Hill coefficient)'='hill',
-                                                                 'Quadratic'="quadratic")),
-                                                  numericInput('plan_hill_coefficient',
-                                                               'Hill coefficient value',
-                                                               value = NULL),
-                                                  numericInput('plan_donor_concentration',
-                                                               'Donor Concentration (required if algorithm is quadratic)',
-                                                               value = NULL),
-                                                  numericInput('plan_min_concentration',
-                                                               'Minimum concentration',
-                                                               value = NULL),
-                                                  numericInput('plan_max_concentration',
-                                                               'Maximum concentration',
-                                                               value = NULL),
-                                                  plotOutput(outputId = "plan_output")
-                                           ),
-                                           fluidRow(column(downloadButton(outputId = 'plan_download', label = "Download Plot"), width=12, offset=0)),
+                                           fluidRow(
+                                               column(width = 3, offset = 1,
+                                                      fluidRow(
+                                                          radioButtons(inputId = 'plan_algorithm',
+                                                                       label = 'Select algorithm for the binding model:',
+                                                                       choices = c('Hyperbolic'='hyperbolic','Hyperbolic (with Hill coefficient)'='hill',
+                                                                                   'Quadratic'="quadratic")))),
+                                               column(width = 2,
+                                                      fluidRow(
+                                                          numericInput(inputId = 'plan_hill_coefficient',width = "140px",
+                                                                       label = 'Hill coefficient',
+                                                                       value = NULL)),
+                                                      fluidRow(
+                                                          numericInput(inputId = 'plan_donor_concentration',width = "140px",
+                                                                       label = 'Probe Concentration',
+                                                                       value = NULL))),
+                                               column(width = 2,
+                                                      fluidRow(
+                                                          numericInput(inputId = 'plan_min_concentration', width = "140px",
+                                                                       label = 'Min concentration',
+                                                                       value = NULL)),
+                                                      fluidRow(
+                                                          numericInput(inputId = 'plan_max_concentration', width = "140px",
+                                                                       label = 'Max concentration',
+                                                                       value = NULL))),
+                                               column(width = 2,
+                                                      offset = 0,
+                                                      fluidRow(
+                                                          numericInput(inputId = 'plan_kd',width = "140px",
+                                                                       label = 'Kd',
+                                                                       value = NULL,
+                                                                       min = 0)),
+                                                      fluidRow(div(style = "height:25px;")),
+                                                      fluidRow(
+                                                          downloadButton(outputId = 'plan_download',
+                                                                         label = "Download Plot")
+                                                      ))),
+                                           plotOutput(outputId = "plan_output"),
                                            fluidRow(column(width=12, offset=0))
                                        )
                               )
